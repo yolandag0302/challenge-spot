8ddffe4f817856800f7e2e0418db85036e21dd1c
Yolanda <yolandag0302@gmail.com>
2022-08-02 22:26:34 -0500

    Adding cvs file with catastro data and CatastroController file

--- app/Http/Controllers/Api/CatastroController.php
+++ app/Http/Controllers/Api/CatastroController.php
@@ -3,39 +3,42 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Catastro;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CatastroController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int $zipCode
+     * @param string $aggregate
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function show(Request $request, int $zipCode, string $aggregate)
     {
-        $catastros = Catastro::all();
+        $constructionType = $request->construction_type;
+        if (!in_array($constructionType, [1, 2, 3, 4, 5, 6, 7])) {
+            abort(404);
+        }
+
+        $payload = DB::select('SELECT "' . $aggregate . '" as type, ' . $aggregate . '(price_unit) AS price_unit, ' . $aggregate . '(price_unit_construction) as price_unit_construction, COUNT(*) AS elements FROM catastros WHERE uso_construccion= ? AND codigo_postal= ?', [$this->getUsoConstruccion($constructionType), $zipCode]);
 
         return response()->json([
             'status' => true,
-            'catastros' => [],
+            'payload' => $payload[0],
         ]);
     }
 
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Catastro  $catastro
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Catastro $catastro)
+    private function getUsoConstruccion(int $constructionType): string
     {
-        return response()->json([
-            'status' => true,
-            'catastro' => $catastro,
-        ]);
+        return match (true) {
+            1 === $constructionType => 'Areas verdes',
+            2 === $constructionType => 'Centro de barrio',
+            3 === $constructionType => 'Equipamiento',
+            4 === $constructionType => 'Habitacional',
+            5 === $constructionType => 'Habitacional y comercial',
+            6 === $constructionType => 'Industrial',
+            default => 'Sin Zonificación',
+        };
     }
 }
